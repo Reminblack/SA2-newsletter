9add613dd5de231a296253e3ded51aa16be592ee
Reminblack <Marinus.C.Visser@gmail.com>
2013-03-06 15:52:45 +0100

    factory and stores + newmessage + nicely ordered

--- SWA2/SWA2/NieuwsBericht.cs
+++ SWA2/SWA2/NieuwsBericht.cs
@@ -5,11 +5,9 @@ using System.Text;
 
 namespace SWA2
 {
-    public abstract class NieuwsBericht
+    public class NieuwsBericht
     {
-        String author;
-        DateTime date;
-
+        String type;
         Header header;
         Content content;
         Footer footer;
---
+++ SWA2/SWA2/factories/AbstractComponentFactory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SWA2
+{
+    public interface AbstractComponentFactory
+    {
+        public Header createHeader();
+
+        public Content createContent();
+
+        public Footer createFooter();
+    }
+}
