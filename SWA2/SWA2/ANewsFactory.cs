03541d130eb040ccfd29ec99002298dc1ede5475
Reminblack <Marinus.C.Visser@gmail.com>
2013-03-06 14:26:57 +0100

    abstract store and abstract newsletter made

---
+++ SWA2/SWA2/ANewsFactory.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SWA2
+{
+    public abstract class ANewsFactory
+    {
+        public abstract NieuwBericht createNieuwsbericht(String type)
+        {
+            NieuwsBericht news = writeNieuwsBericht(type);
+
+            news.maak();
+            news.verifieer();
+            news.post();
+
+            return news;
+        }
+
+        protected abstract NieuwsBericht writeNieuwsBericht(String type)
+        {
+
+        }
+
+    }
+}
---
+++ SWA2/SWA2/NieuwsBericht.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SWA2
+{
+    public abstract class NieuwsBericht
+    {
+        String author;
+        DateTime date;
+
+        Header header;
+        Content content;
+        Footer footer;
+    }
+}
