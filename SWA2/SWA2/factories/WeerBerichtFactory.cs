484f2d04fbd17cc7214dd5892298124fd0934fea
Reminblack <Marinus.C.Visser@gmail.com>
2013-03-06 17:01:25 +0100

    .

---
+++ SWA2/SWA2/factories/WeerBerichtFactory.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SWA2.factories
+{
+    class WeerBerichtFactory : AbstractComponentFactory
+    {
+        public Header createHeader()
+        {
+            Header header = new WeerberichtHeader();
+
+            return header;
+        }
+
+        public Content createContent()
+        {
+            Content content = new WeerberichtContent();
+
+            return content;
+        }
+
+        public Footer createFooter()
+        {
+            Footer footer = new WeerberichtFooter();
+
+            return footer;
+        }
+    }
+}
