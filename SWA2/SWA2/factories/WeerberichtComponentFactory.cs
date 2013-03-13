1faad18d4100d6a4a0f1bfc102a390a82a8679e3
Reminblack <Marinus.C.Visser@gmail.com>
2013-03-13 14:37:34 +0100

    factories aangepast

---
+++ SWA2/SWA2/factories/WeerberichtComponentFactory.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SWA2.factories
+{
+    class WeerberichtComponentFactory : AbstractComponentFactory
+    {
+        public Header createHeader()
+        {
+            IHeader header = new SWA2.products.Headers.Weerberichtheader();
+
+
+            return IHeader;
+        }
+
+        public Content createContent();
+
+        public Footer createFooter();
+    }
+}
