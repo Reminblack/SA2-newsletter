367127f45edeff5786f54dec2a4fe60c65ece139
Bart de Ruijter <Bulldog_233@hotmail.com>
2013-03-13 09:18:41 -0600

    Fix it commit!

---
+++ SWA2/SWA2/products/IContent.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SWA2
+{
+   
+    public interface IContent
+    {
+        void setText(String _nieuwsText);
+        void setImage(String _image);
+    }
+}
