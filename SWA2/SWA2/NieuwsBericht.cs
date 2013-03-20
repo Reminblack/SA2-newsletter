52e5bb59c7cfc454b77c06df9deef1606cec318a
Bart de Ruijter <Bulldog_233@hotmail.com>
2013-03-20 06:52:37 -0600

    Commited Niewsbericht

--- SWA2/SWA2/NieuwsBericht.cs
+++ SWA2/SWA2/NieuwsBericht.cs
@@ -7,9 +7,49 @@ namespace SWA2
 {
     public class NieuwsBericht
     {
-        String type;
-        IHeader header;
-        IContent content;
-        IFooter footer;
+        private String type;
+        private IHeader header;
+        private IContent content;
+        private IFooter footer;
+
+        public void setType(String type)
+        {
+            this.type = type;
+        }
+
+        public String getType()
+        {
+            return type;
+        }
+
+        public void setHeader(IHeader header)
+        {
+            this.header = header;
+        }
+
+        public IHeader getHeader()
+        {
+            return header;
+        }
+
+        public void setContent(IContent content)
+        {
+            this.content = content;
+        }
+
+        public IContent getContent()
+        {
+            return content;
+        }
+
+        public void setFooter(IFooter footer)
+        {
+            this.footer = footer;
+        }
+
+        public IFooter getFooter()
+        {
+            return footer;
+        }
     }
 }
